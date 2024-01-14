db6ac5eee1ce9c2004628e4cb69aadd07ce98927
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2024-01-14 21:54:08 +0100

    Casemaker: Add option to make faces selectable

--- ESPlant-Casemaker/server/static/gltf_renderer.js
+++ ESPlant-Casemaker/server/static/gltf_renderer.js
@@ -4,6 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 /**
+ * TODO extract to class
  * @typedef {Object} Component
  * @property {THREE.Object3D<THREE.Object3DEventMap>} model
  * @property {string} name
@@ -16,8 +17,26 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
  * @property {boolean} selected
  * @property {boolean} highlightable
  * @property {boolean} selectable
+ * @property {boolean} faceSelectable
  * @property {THREE.MeshStandardMaterial} material
  * @property {THREE.LineBasicMaterial} lineMaterial
+ * Box mesh
+ * @property {THREE.MeshStandardMaterial[]} boxMeshMaterials
+ * @property {boolean[]} highlightedBoxMeshMaterials
+ * @property {boolean[]} selectedBoxMeshMaterials
+ * @property {THREE.Mesh} boxMesh
+ * @property {number} boxMeshColor
+ * @property {number} boxMeshOpacity
+ * @property {number} boxMeshEdgeColor
+ * @property {number} highlightedBoxMeshColor
+ * @property {number} highlightedBoxMeshOpacity
+ * @property {number} highlightedBoxMeshEdgeColor
+ * @property {function(number): void} setBoxMeshColor
+ * @property {function(number): void} setBoxMeshOpacity
+ * @property {function(number): void} setBoxMeshEdgeColor
+ * @property {function(number): void} setHighlightedBoxMeshColor
+ * @property {function(number): void} setHighlightedBoxMeshOpacity
+ * @property {function(number): void} setHighlightedBoxMeshEdgeColor
  */
 
 export default class BoardRenderer {
@@ -62,6 +81,13 @@ export default class BoardRenderer {
         this.selectedComponentColor = 0xFFFF00;
         this.highlightedEdgeColor = 0xb3b300;
 
+        this.defaultBoxMeshColor = 0x000000;
+        this.defaultBoxMeshOpacity = 0.2;
+        this.defaultBoxMeshEdgeColor = 0xffff00;
+        this.highlightedBoxMeshColor = 0x0000ff;
+        this.highlightedBoxMeshOpacity = 0.6;
+        this.highlightedBoxMeshEdgeColor = 0x000000;
+
 
         this.onMouseMove = this.onMouseMove.bind(this);
         this.onMouseClick = this.onMouseClick.bind(this);
@@ -77,13 +103,16 @@ export default class BoardRenderer {
         this.renderOnLoad = true;
     }
 
-    centerCamera() {
+    centerModel() {
         // center the model
         const box = new THREE.Box3().setFromObject(this.model);
         const center = box.getCenter(new THREE.Vector3());
         this.model.position.sub(center);
+    }
 
+    centerCamera() {
         // ensure the model fits in the view
+        const box = new THREE.Box3().setFromObject(this.model);
         const size = box.getSize(new THREE.Vector3());
         const maxDimension = Math.max(size.x, size.y, size.z);
         const fitHeightDistance = maxDimension / (2 * Math.atan(Math.PI * this.camera.fov / 360));
@@ -110,6 +139,31 @@ export default class BoardRenderer {
             const board = this.model.children[0];
             for (let i = 0; i < board.children.length; i++) {
                 const componentModel = board.children[i];
+
+                // Create a bounding box for each component with individual faces
+                const componentBox = new THREE.Box3().setFromObject(componentModel);
+                const componentBoxSize = componentBox.getSize(new THREE.Vector3());
+                const componentBoxCenter = componentBox.getCenter(new THREE.Vector3());
+                const boxMeshMaterials = [
+                    new THREE.MeshStandardMaterial({ color: this.defaultBoxMeshColor, transparent: true, opacity: this.defaultBoxMeshOpacity }), // right
+                    new THREE.MeshStandardMaterial({ color: this.defaultBoxMeshColor, transparent: true, opacity: this.defaultBoxMeshOpacity }), // left
+                    new THREE.MeshStandardMaterial({ color: this.defaultBoxMeshColor, transparent: true, opacity: this.defaultBoxMeshOpacity }), // top
+                    new THREE.MeshStandardMaterial({ color: this.defaultBoxMeshColor, transparent: true, opacity: this.defaultBoxMeshOpacity }), // bottom
+                    new THREE.MeshStandardMaterial({ color: this.defaultBoxMeshColor, transparent: true, opacity: this.defaultBoxMeshOpacity }), // front
+                    new THREE.MeshStandardMaterial({ color: this.defaultBoxMeshColor, transparent: true, opacity: this.defaultBoxMeshOpacity }), // back
+                ];
+                const boxMesh = new THREE.Mesh(
+                    new THREE.BoxGeometry(componentBoxSize.x, componentBoxSize.y, componentBoxSize.z),
+                    boxMeshMaterials
+                );
+                boxMesh.position.copy(componentBoxCenter);
+                const boxMeshEdges = new THREE.EdgesGeometry(boxMesh.geometry);
+                const boxMeshEdgesMaterial = new THREE.LineBasicMaterial({ color: this.defaultBoxMeshEdgeColor });
+                const boxMeshEdgesMesh = new THREE.LineSegments(boxMeshEdges, boxMeshEdgesMaterial);
+                boxMeshEdgesMesh.position.copy(componentBoxCenter);
+                componentModel.add(boxMeshEdgesMesh);
+                this.scene.add(boxMesh);
+
                 /**
                  * @type {Component}
                  */
@@ -131,8 +185,37 @@ export default class BoardRenderer {
                     selected: false,
                     highlightable: true,
                     selectable: true,
+                    faceSelectable: true, // TODO set false
                     material: new THREE.MeshStandardMaterial({ color: this.defaultComponentColor }),
-                    lineMaterial: new THREE.LineBasicMaterial({ color: this.defaultEdgeColor })
+                    lineMaterial: new THREE.LineBasicMaterial({ color: this.defaultEdgeColor }),
+                    boxMeshMaterials: boxMeshMaterials,
+                    highlightedBoxMeshMaterials: [false, false, false, false, false, false],
+                    selectedBoxMeshMaterials: [false, false, false, false, false, false],
+                    boxMesh: boxMesh,
+                    boxMeshColor: this.defaultBoxMeshColor,
+                    boxMeshOpacity: this.defaultBoxMeshOpacity,
+                    boxMeshEdgeColor: this.defaultBoxMeshEdgeColor,
+                    highlightedBoxMeshColor: this.highlightedBoxMeshColor,
+                    highlightedBoxMeshOpacity: this.highlightedBoxMeshOpacity,
+                    highlightedBoxMeshEdgeColor: this.highlightedBoxMeshEdgeColor,
+                    setBoxMeshColor: function (color) {
+                        this.boxMeshColor = color;
+                        this.boxMeshMaterials.forEach((material) => {
+                            material.color.set(color);
+                        });
+                    },
+                    setBoxMeshOpacity: function (opacity) {
+                        this.boxMeshOpacity = opacity;
+                        this.boxMeshMaterials.forEach((material) => {
+                            material.opacity = opacity;
+                        });
+                    },
+                    setBoxMeshEdgeColor: function (color) {
+                        this.boxMeshEdgeColor = color;
+                        this.boxMeshMaterials.forEach((material) => {
+                            material.color.set(color);
+                        });
+                    },
                 };
 
                 this.components.push(component);
@@ -208,7 +291,6 @@ export default class BoardRenderer {
      * @param {{x: number, y: number}} windowPosition
      */
     getRaycastComponent(windowPosition) {
-
         const rect = this.container.getBoundingClientRect();
         const containerPosition = new THREE.Vector2(
             ((windowPosition.x - rect.left) / rect.width) * 2 - 1,
@@ -234,6 +316,53 @@ export default class BoardRenderer {
         return undefined;
     }
 
+    /**
+     * @param {{x: number, y: number}} windowPosition
+     * @param {Component} component 
+     * @returns {THREE.MeshStandardMaterial | undefined}
+     * The material of the intersected face of the component's box mesh
+     */
+    getRaycastComponentBoxMeshMaterial(windowPosition, component) {
+        const rect = this.container.getBoundingClientRect();
+        const containerPosition = new THREE.Vector2(
+            ((windowPosition.x - rect.left) / rect.width) * 2 - 1,
+            -((windowPosition.y - rect.top) / rect.height) * 2 + 1
+        )
+
+        function normalToMeshMaterialIndex(normal) {
+            if (normal === undefined) {
+                return undefined;
+            }
+            if (normal.x === 1) {
+                return 0;
+            } else if (normal.x === -1) {
+                return 1;
+            } else if (normal.y === 1) {
+                return 2;
+            } else if (normal.y === -1) {
+                return 3;
+            } else if (normal.z === 1) {
+                return 4;
+            } else if (normal.z === -1) {
+                return 5;
+            }
+        }
+
+        const raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(containerPosition, this.camera);
+        const intersects = raycaster.intersectObject(component.boxMesh, true);
+        if (intersects.length > 0) {
+            let intersect = intersects[0];
+            if (intersect !== null && intersect !== undefined) {
+                const meshMaterialIndex = normalToMeshMaterialIndex(intersect.face?.normal);
+                if (meshMaterialIndex !== undefined) {
+                    return component.boxMeshMaterials[meshMaterialIndex];
+                }
+            }
+        }
+        return undefined;
+    }
+
     onMouseMove(event) {
         this.mouseMovedAfterMouseDown = true;
         const mousePosition = {
@@ -242,6 +371,15 @@ export default class BoardRenderer {
         };
 
         const component = this.getRaycastComponent(mousePosition);
+        if (component?.faceSelectable) {
+            const componentBoxMeshMaterial = this.getRaycastComponentBoxMeshMaterial(mousePosition, component);
+            this.container.dispatchEvent(new CustomEvent('component-face-hover', {
+                detail: {
+                    component: component,
+                    material: componentBoxMeshMaterial,
+                },
+            }));
+        }
         if (component !== undefined) {
             if (this.areComponentsHighlightable) {
                 if (!component.highlighted && component.highlightable) {
